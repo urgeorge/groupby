fb0ad1c479092a067e3f4c146d6a84430b0049e1
Grzegorz <grzegorzbielesza@gmail.com>
2022-01-26 22:06:34 +0100

    Backend project generated

---
+++ groupby-backend/src/main/java/com/groupby/groupbybackend/GroupbyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.groupby.groupbybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupbyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupbyBackendApplication.class, args);
+	}
+
+}
