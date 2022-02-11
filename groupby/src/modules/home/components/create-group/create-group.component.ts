7abe3cec908f86de27acc811e6a23cf0de55700a
Grzegorz <grzegorzbielesza@gmail.com>
2022-02-11 22:06:24 +0100

    initial creating group form added

--- groupby/src/modules/home/components/create-group/create-group.component.ts
+++ groupby/src/modules/home/components/create-group/create-group.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {faLock} from "@fortawesome/free-solid-svg-icons";
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faCalendarAlt} from "@fortawesome/free-solid-svg-icons";
+import {faVenusMars} from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: 'app-create-group',
   templateUrl: './create-group.component.html',
   styleUrls: ['./create-group.component.css']
 })
 export class CreateGroupComponent implements OnInit {
+
+  faUsername = faUser;
+  faPassword = faLock;
+  faMail = faEnvelope;
+  faCalendar = faCalendarAlt;
+  faGender = faVenusMars;
 
   constructor() { }
 
--- groupby/src/modules/home/home.module.ts
+++ groupby/src/modules/home/home.module.ts
@@ -4,12 +4,14 @@ import * as homeServices from "../home/services";
 import * as homeGuards from "../home/guards";
 import * as homeComponents from "../home/components";
 import {NavigationModule} from "../navigation/navigation.module";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 
 @NgModule({
   imports: [
     CommonModule,
     NavigationModule,
+    FontAwesomeModule,
   ],
   providers: [...homeServices.services, ...homeGuards.guards],
   declarations: [...homeComponents.components],
